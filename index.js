067a834c5989c8ce60d153e2ab19695f59c19d76
idlviv <idlviv@rambler.ru>
2017-01-11 20:49:43 +0200

    localStorage, JSON - final

--- index.js
+++ index.js
@@ -2,19 +2,35 @@
 var state = {
   items:[]
 };
-
+var d = localStorage.getItem('myLocalData');
+if (d) {
+  state = JSON.parse(d);
+  console.log(d);
+  console.log(state);
+}
 window.addEventListener('load', function wListener() {
   'use strict';
   var list = document.querySelector('ul');
   var button = document.querySelector('button');
+  var buttonClear = document.querySelector('#buttonClear');
   var input = document.querySelector('input');
-  button.addEventListener('click',function (){
-    var inputedValue = input.value;
-    var newElement = document.createElement('li');
-    newElement.innerText = inputedValue;
-    list.appendChild(newElement);
+  console.log (state.items.length);
+  update();
+ 
+  function update (){
     input.value = '';
-    state.items.push(inputedValue);
-
-  })
+    list.innerHTML = '';
+    for (var i = 0; i < state.items.length; i++){
+      var newElement = document.createElement('li');
+      newElement.innerText = state.items[i];
+      list.appendChild(newElement);
+    }
+  }
+  button.addEventListener('click', add);
+  function add (){
+    var inputtedValue = input.value;
+    state.items.push(inputtedValue);
+    localStorage.setItem('myLocalData',JSON.stringify(state));
+    update();
+  }
 });
